60c086221c8f0c9eca7f205243ffce3bd6598578
cgq <cgq_rain@163.com>
2024-04-02 17:48:14 +0800

    更改消息接受和输出类型

--- src/main/java/com/tencent/wxcloudrun/controller/MessageReceiveController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/MessageReceiveController.java
@@ -6,12 +6,13 @@ import com.tencent.wxcloudrun.dto.MessageResponse;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/message")
 public class MessageReceiveController {
 
-    @PostMapping("receive")
+/*    @PostMapping("receive")
     public Object receive(@RequestBody MessageRequest request){
         if (request.getAction()!=null && request.getAction().equals("CheckContainerPath")){
             System.out.println("收到预检请求");
@@ -28,5 +29,21 @@ public class MessageReceiveController {
             System.out.println(messageResponse);
             return messageResponse;
         }
+    }*/
+
+    @PostMapping("receive")
+    public Object receive(@RequestBody Map<String,Object> request){
+
+        System.out.println("接受消息，消息信息为 " + request.toString());
+        System.out.println("我准备回复消息了");
+        MessageResponse messageResponse = new MessageResponse();
+        messageResponse.setToUserName(request.get("FromUserName").toString());
+        messageResponse.setFromUserName(request.get("ToUserName").toString());
+        messageResponse.setMsgType("text");
+        messageResponse.setContent("已经收到消息为" + request.get("content") +",返回给你 收到");
+        messageResponse.setCreateTime(new Date().getTime() + "");
+        System.out.println(messageResponse);
+        return messageResponse;
+
     }
 }
