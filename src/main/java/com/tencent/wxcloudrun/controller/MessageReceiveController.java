202de642e2b2d0616d350e9f19486251cf7b1a61
cgq <cgq_rain@163.com>
2024-04-02 18:21:53 +0800

    测试使用response返回

--- src/main/java/com/tencent/wxcloudrun/controller/MessageReceiveController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/MessageReceiveController.java
@@ -1,10 +1,16 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tencent.wxcloudrun.config.ApiResponse;
 import com.tencent.wxcloudrun.dto.MessageRequest;
 import com.tencent.wxcloudrun.dto.MessageResponse;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -39,7 +45,7 @@ public class MessageReceiveController {
     }*/
 
     @PostMapping("receive")
-    public Object receive(@RequestBody Map<String,Object> request){
+    public Object receive(@RequestBody Map<String,Object> request, HttpServletResponse response) throws IOException {
 
         // {ToUserName=gh_bd70fc67ee13,
         // FromUserName=o0K-V6oEtzKEGWaB1SJsKQm80bew,
@@ -49,14 +55,17 @@ public class MessageReceiveController {
         // MsgId=24510694633440943}
         System.out.println("接受消息，消息信息为 " + request.toString());
         System.out.println("我准备回复消息了");
-        Map<String,Object> response = new HashMap<>();
-        response.put("ToUserName",request.get("FromUserName").toString());
-        response.put("FromUserName",request.get("ToUserName").toString());
-        response.put("MsgType",request.get("FromUserName").toString());
-        response.put("Content",request.get("content") +",返回给你 收到");
-        response.put("CreateTime",new Date().getTime() + "");
-        System.out.println("我准备回复消息了 " + response);
-        return response;
+        Map<String,Object> repmap = new HashMap<>();
+        repmap.put("ToUserName",request.get("FromUserName").toString());
+        repmap.put("FromUserName",request.get("ToUserName").toString());
+        repmap.put("MsgType",request.get("FromUserName").toString());
+        repmap.put("Content",request.get("content") +",返回给你 收到");
+        repmap.put("CreateTime",new Date().getTime() + "");
+        System.out.println("我准备回复消息了 " + repmap);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String s = objectMapper.writeValueAsString(repmap);
+        response.getWriter().write(s);
+        return repmap;
 
     }
 }
